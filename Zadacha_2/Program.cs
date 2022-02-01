547ef164949737dbc59e4d9563c7185a06dc2d42
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-01 14:35:02 +0400

    Решили задачу 2, поиск минимума и максимума

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write("Введите первое число-> ");
+string? numberStr_1 = Console.ReadLine();
+
+Console.Write("Введите второе число-> ");
+string? numberStr_2 = Console.ReadLine();
+
+int number_1 = int.Parse(numberStr_1); //преобразование из строкой переменной в int
+int number_2 = int.Parse(numberStr_2); //преобразование из строкой переменной в int
+
+if (number_1 > number_2)
+Console.WriteLine($"{number_1} > {number_2}, соответственно: max -{number_1}, min - {number_2}.");
+if (number_1 < number_2)
+Console.WriteLine($"{number_1} < {number_2}, соответственно: max -{number_2}, min - {number_1}.");
+if (number_1 == number_2)
+Console.WriteLine($"Числа равны: {number_1} = {number_2}, минимальное и максимальное значение отсутствует.");
