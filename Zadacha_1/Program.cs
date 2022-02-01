784e2f751f54250dd87a1e02ea30791b3d18babf
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-01 13:46:51 +0400

    Решили задачу 1 (является первое число квадратом второго?)

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли первое число квадратом второго
+
+Console.Write("Введите первое число-> ");
+string? numberStr_1 = Console.ReadLine();
+
+Console.Write("Введите второе число-> ");
+string? numberStr_2 = Console.ReadLine();
+
+int number_1 = int.Parse(numberStr_1); //преобразование из строкой переменной в int
+int number_2 = int.Parse(numberStr_2); //преобразование из строкой переменной в int
+
+if (number_1 == number_2*number_2)
+Console.WriteLine($"Квадрат числа: {number_2} = {number_1}");
+else
+Console.WriteLine($"Число: {number_1} не является квадратом числа {number_2}");
