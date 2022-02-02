9488ccb0fcf23ba5e96b8428b8a8b63e4a5b6701
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-02 21:57:54 +0400

    определение четного числа

---
+++ Zadacha_5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Является ли число четным
+int number = new Random().Next(1, 20); // Случайное число
+Console.WriteLine(number);
+int ost = 0;
+ost = number%2;
+if (ost == 0)
+Console.WriteLine($"Число {number} четное.");
+else
+Console.WriteLine($"Число {number} нечетное.");
