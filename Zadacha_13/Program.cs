f79532196d1f5c4bd79d9ba9d1e62bf183f47115
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-04 22:25:49 +0400

    13. Выяснить, кратно ли число заданному, если нет, то вывести остаток

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int GetRandomNumber ()  //Функция для полчения случайного числа
+{
+   int randomNumber = new Random().Next(20,100);
+   return randomNumber;
+}
+
+void IsMultiple (int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber)
+    {
+        int result = firstNumber % secondNumber;
+        if (result == 0)
+            Console.WriteLine($"Число {firstNumber} кратно {secondNumber}, остаток {result}");
+        else
+            Console.WriteLine($"Число {firstNumber} не кратно {secondNumber}, остаток {result}");
+    }
+    else 
+    {        
+        int result = firstNumber % secondNumber;
+        if (result == 0)
+            Console.WriteLine($"Число {secondNumber} кратно {firstNumber}, остаток {result}");
+        else
+            Console.WriteLine($"Число {secondNumber} не кратно {firstNumber}, остаток {result}");
+    }
+}
+int number = GetRandomNumber(); // Получили какое-то случайное число
+Console.WriteLine($"Первое случайное число: {number}");
+
+int delitel = GetRandomNumber(); //Случайное второе число
+
+Console.WriteLine($"Второе случайное число: {delitel}");
+
+IsMultiple(number, delitel);
