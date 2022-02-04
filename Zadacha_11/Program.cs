7459f5bb76e21455f684d3a18554c8af0a805f8f
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-04 22:23:44 +0400

    10. Наибольшая цифра числа из отрезка [10, 99]

---
+++ Zadacha_11/Program.cs
@@ -0,0 +1,21 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int GetRandomNumber ()
+{
+   int randomNumber = new Random().Next(10,100);
+   return randomNumber;
+}
+
+int number = GetRandomNumber();
+Console.WriteLine(number);
+int numberOne =  number / 10;
+int numberTwo = number % 10;
+
+if (numberOne > numberTwo)
+    Console.WriteLine($"Наибольшее из цифр {numberOne}");
+else
+    Console.WriteLine($"Наибольшее из цифр {numberTwo}");
+
+
+
+
