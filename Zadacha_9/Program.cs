34c3e36ce471948ebdb011d70d1ea7fefd33be3f
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-04 22:21:47 +0400

    9. Показать последнюю цифру трехзначного числа

---
+++ Zadacha_9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int Get(int number)   // Метод для деления числа и нахождения остатка? который будет последней цифрой в числе
+{
+    return number % 10;
+}
+
+int GetConsol(int min, int max) // Число техзначное?
+{
+    int result = 0;
+    bool isError = true;
+    while (isError)
+    {
+        Console.Write($"Введите целое число от {min} по {max}: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out result))  //Для предупреждения введения неправильного числа
+        isError = result < min || result > max;
+    }
+return result;
+}
+
+int number = GetConsol (100, 999);
+int LastDigit = Get(number);
+Console.WriteLine(LastDigit);
+
+
