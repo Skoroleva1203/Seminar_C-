c77c9f7f995c758152e9d7bbfd836bf7a8d3f5aa
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-02-13 22:07:23 +0400

    30. Показать кубы чисел, заканчивающихся на четную цифру

---
+++ Zadacha_30/Program.cs
@@ -0,0 +1,13 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+ 
+int number = 1;
+int num = new Random().Next(1, 50); // Создали объект для генерации чисел
+Console.WriteLine ($"Cгенерировано случайное число {num}");
+Console.WriteLine ($"Кубы чисел в пределах от 1 до {num} заканчиваются на четную цифру: ");
+   
+for (number=1; number<= num; number++)
+{
+    double cube = Math.Pow(number, 3); // Заданное число возвели в куб
+    if ((cube%2) == 0) // Посик четного числа 
+    Console.WriteLine ($"{number} в кубе = {cube}");  
+}
