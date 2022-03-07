9f7413e388e069b9f715e09558a670911dd6bd2a
Koroleva Svetlana <korolewq.sm1@yandex.ru>
2022-03-07 11:22:15 +0400

    71. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.

---
+++ Zadacha_71/Program.cs
@@ -0,0 +1,22 @@
+﻿// 71. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int GetRandomNumber ()  //Функция для полчения случайного числа
+{
+   int randomNumber = new Random().Next(0,5);
+   return randomNumber;
+}
+
+int FAkkermana(int a, int b) //Метод вычисления функции Аккермана
+{
+    if (a == 0)
+        return  b + 1;
+    else
+        if ((a > 0) && (b == 0))
+        return FAkkermana(a - 1, 1);
+    else
+        return FAkkermana(a - 1, FAkkermana(a, b-1));
+}
+
+int m = GetRandomNumber(); 
+int n = GetRandomNumber(); 
+Console.WriteLine($"\nВычислим функцию Аккермана для А ({m}, {n})\n");
+Console.WriteLine($"А ({m}, {n}) = {FAkkermana(n, m)}");
